29e0b219746a8a95dcf74f5be460110dbfe0698c
GauravKumar-ai <gopal.marwari@gmail.com>
2022-11-07 11:13:08 +0530

    All Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sensor",
+    version="0.0.2",
+    author="gaurav",
+    author_email="gopal.marwari@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
